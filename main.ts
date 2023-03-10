d4888b4612877ec7ebfae0c75720670986cdf743
vdm-julieke <124454367+vdm-julieke@users.noreply.github.com>
2023-03-10 13:22:05 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,14 +1,29 @@
 basic.forever(function () {
     if (input.compassHeading() > 45 && input.compassHeading() < 135) {
-        basic.showArrow(ArrowNames.East)
+        basic.showArrow(ArrowNames.West)
     }
     if (input.compassHeading() > 135 && input.compassHeading() < 225) {
         basic.showArrow(ArrowNames.South)
     }
     if (input.compassHeading() > 225 && input.compassHeading() < 315) {
-        basic.showArrow(ArrowNames.West)
+        basic.showArrow(ArrowNames.East)
     }
-    if (input.compassHeading() > 315 && input.compassHeading() < 45) {
-        basic.showArrow(ArrowNames.North)
+    if (input.compassHeading() > 315) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (input.compassHeading() < 45) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
     }
 })
