e780e573a07ac37e8468accc422b63ddc2705d24
vdm-julieke <124454367+vdm-julieke@users.noreply.github.com>
2023-03-13 10:27:07 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 let Richting = 0
 basic.forever(function () {
     Richting = input.compassHeading()
+})
+basic.forever(function () {
     if (Richting > 45 && Richting < 135) {
         basic.showLeds(`
             . . # . .
@@ -15,6 +17,15 @@ basic.forever(function () {
     }
     if (Richting > 225 && Richting < 315) {
         basic.showArrow(ArrowNames.East)
+    }
+    if (Richting > 315) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
     }
     if (Richting < 45) {
         basic.showLeds(`
